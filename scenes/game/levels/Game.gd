732e4b8579cdcda07aa284142e3a00373f7affbd
Qaleka <91965947+Qaleka@users.noreply.github.com>
2024-06-30 22:51:42 +0300

    Ура, комит мой

--- scenes/game/levels/Game.gd
+++ scenes/game/levels/Game.gd
@@ -8,47 +8,107 @@ const knights_names = [
 ]
 const ghost_name = "Чёрный Дух"
 
+@onready var container = $HSplitContainer/ScrollContainer/VBoxContainer
+@onready var one_player = preload("res://scenes/game/levels/game_players_list/PlayerElem.tscn")
+@onready var timer = $Vote_timer
+@onready var vote_kick = $HSplitContainer/VBoxContainer/MarginContainer
+
 var level = [[0, 0, 0],
 			 [0, 0, 0],
 			 [0, 0, 0]]
+
+
 
 var startPosX: int = 0
 var startPosY: int = 0
 var endPosX: int = 2
 var endPosY: int = 2
 
+var posX:int = 0
+var posY:int = 0
+
 var choiceX: int = -1
 var choiceY: int = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	vote_kick.hide()
 	# Preconfigure game.
-
+	Lobby.player_info.status = "✓"
+	rpc_id(1,"update_info", multiplayer.get_unique_id(), Lobby.player_info)
 	Lobby.player_loaded.rpc_id(1) # Tell the server that this peer has loaded.
+	get_tree().paused = true
+
+func add_label_player(player_id):
+	var label = one_player.instantiate()
+	label._set_name(str(player_id))
+	label._set_status(Lobby.players[player_id].status)
+	container.add_child(label)
+
+@rpc("authority", "call_local", "reliable")
+func update_data(player_id,data):
+	#print(player_id, " ", data)
+	Lobby.players[player_id] = data.duplicate(true)
+	
+@rpc("any_peer", "call_local", "reliable")
+func update_info(player_id,data):
+	#print(player_id, " ", data)
+	#Lobby.players[player_id] = data
+	for player in Lobby.players:
+		rpc_id(player,"update_data", player_id, Lobby.player_info)
+
+func update_labels():
+	for child in container.get_children():
+		child.queue_free()
+	for player in Lobby.players:
+		add_label_player(player)
+		
+@rpc("any_peer", "call_local", "reliable")
+func update_level(y,x,delta):
+	level[y][x] += delta
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$HSplitContainer/CenterContainer/GridContainer/Button1.set_text(str(level[0][0]))
-	$HSplitContainer/CenterContainer/GridContainer/Button2.set_text(str(level[0][1]))
-	$HSplitContainer/CenterContainer/GridContainer/Button3.set_text(str(level[0][2]))
-	$HSplitContainer/CenterContainer/GridContainer/Button4.set_text(str(level[1][0]))
-	$HSplitContainer/CenterContainer/GridContainer/Button5.set_text(str(level[1][1]))
-	$HSplitContainer/CenterContainer/GridContainer/Button6.set_text(str(level[1][2]))
-	$HSplitContainer/CenterContainer/GridContainer/Button7.set_text(str(level[2][0]))
-	$HSplitContainer/CenterContainer/GridContainer/Button8.set_text(str(level[2][1]))
-	$HSplitContainer/CenterContainer/GridContainer/Button9.set_text(str(level[2][2]))
+	$HSplitContainer/VBoxContainer/Label.set_text("Время до конца голосвания: " + str(snapped(timer.time_left, 1)))
+	update_labels()
+
+	for y in range(0,3):
+		for x in range(0,3):
+			var button = $HSplitContainer/VBoxContainer/CenterContainer/GridContainer.get_child(x + y * 3)
+			button.set_text(str(level[y][x]))
+			if absi(x - posX) == 1 && absi(y - posY) == 0 && $HSplitContainer/VBoxContainer/Label.visible:
+				button.disabled = false
+			elif absi(x - posX) == 0 && absi(y - posY) == 1 && $HSplitContainer/VBoxContainer/Label.visible:
+				button.disabled = false
+			else:
+				button.disabled = true
+				button.set_text("")
+			
+			if x == posX && y == posY:
+				button.self_modulate = Color(255, 0, 0)
+			else:
+				button.self_modulate = Color(255, 255, 255)
+				
+@rpc("any_peer", "call_local", "reliable")
+func unpause_game():
+	get_tree().paused = false
 
 # Called only on the server.
 func start_game():
-	pass # All peers are ready to receive RPCs in this scene.
+	for player in Lobby.players:
+		rpc_id(player, "unpause_game")
 
-func _on_choice(x: int, y: int):
+func _on_choice(y: int, x: int):
 	if choiceX != -1:
-		level[choiceX][choiceY] -= 1
+		#level[choiceX][choiceY] -= 1
+		for player in Lobby.players:
+			rpc_id(player,"update_level", choiceY, choiceX, -1)
 	choiceX = x
 	choiceY = y
-	level[choiceX][choiceY] += 1
-	print(x, "; ", y)
+	#level[choiceX][choiceY] += 1
+	for player in Lobby.players:
+		rpc_id(player,"update_level", choiceY, choiceX, 1)
+	print(y, "; ", x)
 
 func _on_button_1_pressed():
 	_on_choice(0, 0)
@@ -76,3 +136,37 @@ func _on_button_8_pressed():
 
 func _on_button_9_pressed():
 	_on_choice(2, 2)
+
+func _on_vote_timer_timeout():
+	var max = 0
+	var maxX = posX
+	var maxY = posY
+	if maxX < 2:
+		maxX += 1
+	else:
+		maxX -= 1
+	for y in range(0,3):
+		for x in range(0,3):
+			if level[y][x] > max:
+				max = level[y][x]
+				maxX = x
+				maxY = y
+	posX = maxX
+	posY = maxY
+	for y in range(0,3):
+		for x in range(0,3):
+			level[y][x] = 0
+	choiceX = -1
+	choiceY = -1
+	$HSplitContainer/VBoxContainer/Label.hide()
+	vote_kick.show()
+	$HSplitContainer/VBoxContainer/MarginContainer/Node2D2.show()
+
+
+func _on_node_2d_2_visibility_changed():
+	print("changechan")
+	if not $HSplitContainer/VBoxContainer/MarginContainer/Node2D2.visible:
+		$HSplitContainer/VBoxContainer/Label.show()
+		vote_kick.hide()
+		timer.start(10)
+	
--- scenes/game/levels/choice/choice_script.gd
+++ scenes/game/levels/choice/choice_script.gd
@@ -1,54 +1,78 @@
-extends Node2D
-class_name Choice
+extends Control
 
-var level = [[0, 0, 0],
-			 [0, 0, 0],
-			 [0, 0, 0]]
-
-var startPosX = 0
-var startPosY = 0
-var endPosX = 2
-var endPosY = 2
-
-@onready var button1 = $Button
-@onready var button2 = $Button2
-@onready var button3 = $Button3
 @onready var timer = $Timer
+@onready var container = $VBoxContainer/HBoxContainer
+
+var votes = {}
+var buttons = []
+var choice_id = "-1"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$VBoxContainer/Player_to_kick.set_text("")
 	Lobby.player_info.test = 2
+	for child in container.get_children():
+		child.queue_free()
+	
+	for player in Lobby.players:
+		var button = Button.new()
+		button.pressed.connect(is_button_pressed.bind(str(player)))
+		buttons.append(button)
+		button.set_text(str(player))
+		votes[str(player)] = 0
+		button.size_flags_horizontal = Control.SIZE_EXPAND_FILL 
+		container.add_child(button)
+	print(votes)
 	#rpc_id(1,"update_info", multiplayer.get_unique_id(), Lobby.player_info.test)
 	#Lobby.player_loaded.rpc_id(1)
-
-##Сделать ловушки на случайные комнаты
-##Функцию голосования, но как получить вариант выбора?
+	
+@rpc("any_peer", "call_local", "reliable")
+func update_votes(player_name, delta):
+	votes[player_name] += delta
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Time_to_choice.set_text(str(snapped(timer.time_left, 0.1)))
+	$VBoxContainer/Time_to_choice.set_text(str(snapped(timer.time_left, 0.1)))
+	#print(Lobby.players)
 	
-	print(Lobby.players)
+func is_button_pressed(id):
+	if choice_id != "-1":
+		for player in Lobby.players:
+			rpc_id(player,"update_votes", id, -1)
+	choice_id = id
+	for player in Lobby.players:
+		rpc_id(player,"update_votes", id, 1)
+	print("Vote for: " + id)
+	
+	
 	#print(multiplayer.get_remote_sender_id(), multiplayer.get_unique_id())
 	#var local_player_id = get_tree().get_network_unique_id()
-	
-@rpc("authority", "call_local", "reliable")
-func update_data(player_id,data):
-	#print(player_id, " ", data)
-	Lobby.players[player_id] = data
-	
-@rpc("any_peer", "call_local", "reliable")
-func update_info(player_id,data):
-	#print(player_id, " ", data)
-	#Lobby.players[player_id] = data
-	for player in Lobby.players:
-		#print(player)
-		rpc_id(player,"update_data", player_id, Lobby.player_info)
-
 
 func start_game():
 	print("hui")
 
 func _on_timer_timeout():
-	Lobby.player_info.suka = [1, 2 ,3]
-	rpc_id(1,"update_info", multiplayer.get_unique_id(), Lobby.player_info)
+	var max = -1
+	var kicked_player = ""
+	for button in buttons:
+		if votes[button.text] > max:
+			max = votes[button.text]
+			kicked_player = button.text
+	if max == 0:
+		$VBoxContainer/Player_to_kick.set_text("Выбранный игрок: никто")
+	else:
+		$VBoxContainer/Player_to_kick.set_text("Выбранный игрок: " + kicked_player)
+		#print(int(kicked_player))
+		Lobby.players[int(kicked_player)].status = "✖"
+	$Show_kick_timer.start(3)
+	#rpc_id(1,"update_info", multiplayer.get_unique_id(), Lobby.players)
+
+
+func _on_visibility_changed():
+	if visible:
+		timer.start(10)
+		$VBoxContainer/Player_to_kick.set_text("")
+
+
+func _on_show_kick_timer_timeout():
+	visible = not visible
